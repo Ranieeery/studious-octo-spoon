ce732dc4f7485fc19c55274793089791efed398a
Ranieeery <raniery2003@hotmail.com>
2024-12-31 21:35:37 -0300

    feat: enhance main.py to format product descriptions as HTML and display output; update requirements.txt to include IPython

--- OpenAI/prompt engineering/interative/main.py
+++ OpenAI/prompt engineering/interative/main.py
@@ -1,4 +1,5 @@
 import openai
+from IPython.display import display, HTML
 import os
 from openai import OpenAI
 
@@ -70,11 +71,28 @@ Write a product description based on the information
 provided in the technical specifications delimited by 
 triple backticks.
 
-Use at most 50 words.
+The description is intended for furniture retailers, 
+so should be technical in nature and focus on the 
+materials the product is constructed from.
+
+At the end of the description, include every 7-character 
+Product ID in the technical specification.
+
+After the description, include a table that gives the 
+product's dimensions. The table should have two columns.
+In the first column include the name of the dimension. 
+In the second column include the measurements in inches only.
+
+Give the table the title 'Product Dimensions'.
+
+Format everything as HTML that can be used in a website. 
+Place the description in a <div> element.
 
 Technical specifications: ```{fact_sheet_chair}```
 """
+
 response = get_completion(prompt)
 print(response)
 
 print(len(response.split()))
+display(HTML(response))
