89cdc4a94277a0e3e14016cc4a1ac79c1f595690
Ranieeery <raniery2003@hotmail.com>
2025-01-05 03:18:58 -0300

    refactor: simplify node creation and improve output display in addTwoNumbers method

--- LeetCode/Java/2. Add Two Numbers/Solution.java
+++ LeetCode/Java/2. Add Two Numbers/Solution.java
@@ -4,24 +4,21 @@ class Solution {
         ListNode head = new ListNode(0);
         ListNode node = head;
 
-        while (l1  != null || l2  != null || index != 0) {
+        while (l1 != null || l2 != null || index != 0) {
             int num1 = (l1 != null) ? l1.val : 0;
             int num2 = (l2 != null) ? l2.val : 0;
 
             int num = (num1 + num2 + index) % 10;
             index = (num1 + num2 + index) / 10;
 
-            ListNode newNode = new ListNode(num);
-            node.next = newNode;
+            node.next = new ListNode(num);
             node = node.next;
 
             l1 = (l1 != null) ? l1.next : null;
             l2 = (l2 != null) ? l2.next : null;
         }
 
-        ListNode result = head.next;
-        head.next = null;
-        return result;
+        return head.next;
     }
 
     public void main(String[] args) {
@@ -32,7 +29,11 @@ class Solution {
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
-
-        System.out.println(addTwoNumbers(l1, l2));
+        
+        ListNode result = addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
     }
 }
